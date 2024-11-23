b9f411c85839e184375a848b1c21b292a94a013b
waleeedkhalid <axd.edits@gmail.com>
2024-11-23 16:32:46 +0300

    Added new helper methods

--- src/Process/Document.java
+++ src/Process/Document.java
@@ -5,7 +5,7 @@ import List.LinkedList;
 public class Document {
     private int docId;
     private String content;
-    private LinkedList<String> words;
+    private LinkedList<Word> words;
 
     // This is the constructor of the Document class - O(1)
     public Document(int docId, String content) {
@@ -24,12 +24,12 @@ public class Document {
     }
 
     // This method is used to get the words of the document - O(1)
-    public LinkedList<String> getWords() {
+    public LinkedList<Word> getWords() {
         return words;
     }
 
     // This method is used to set the words of the document - O(1)
-    public void setWords(LinkedList<String> words) {
+    public void setWords(LinkedList<Word> words) {
         this.words = words;
     }
 
@@ -48,27 +48,104 @@ public class Document {
         try {
             list.findFirst();
             while (!list.last()) {
-                LinkedList<String> words = list.retrieve().getWords();
+                LinkedList<Word> words = list.retrieve().getWords();
                 words.findFirst();
                 System.out.print("DocumentId " + list.retrieve().getDocId() + ": ");
                 while (!words.last()) {
-                    System.out.print(words.retrieve() + " ");
+                    System.out.print(words.retrieve().getWord() + " ");
                     words.findNext();
                 }
-                System.out.print(words.retrieve() + " ");
+                System.out.print(words.retrieve().getWord() + " ");
                 list.findNext();
                 System.out.println();
             }
-            LinkedList<String> words = list.retrieve().getWords();
+            LinkedList<Word> words = list.retrieve().getWords();
             words.findFirst();
             System.out.print("DocumentId " + list.retrieve().getDocId() + ": ");
             while (!words.last()) {
-                System.out.print(words.retrieve() + " ");
+                System.out.print(words.retrieve().getWord() + " ");
                 words.findNext();
             }
-            System.out.print(words.retrieve() + " ");
+            System.out.print(words.retrieve().getWord()+ " ");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    // This method is used to print the number of tokens in the list of documents - O(n)
+    static public void printNumberOfTokens(LinkedList<Document> documents) {
+        int totalTokens = 0;
+
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            String content = doc.getContent();
+            String[] wordsList = content.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s");
+            LinkedList<String> words = new LinkedList<>();
+            for(String word : wordsList) {
+                words.insert(word);
+            }
+
+            totalTokens += words.size();
+            documents.findNext();
+        }
+
+        Document doc = documents.retrieve();
+        String content = doc.getContent();
+        String[] wordsList = content.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s");
+        LinkedList<String> words = new LinkedList<>();
+        for(String word : wordsList) {
+            words.insert(word);
+        }
+
+        totalTokens += words.size();
+
+        System.out.println("Number of tokens: " + totalTokens);
+    }
+
+    // This method is used to print the number of unique tokens in the list of documents - O(n^2)
+    static public void printNumberOfUniqueTokens(LinkedList<Document> documents) {
+        int totalUniqueTokens = 0;
+
+        LinkedList<String> uniqueWords = new LinkedList<>();
+
+
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            String content = doc.getContent();
+            String[] wordsList = content.toLowerCase().replaceAll("[^a-zA-Z0-9\\s+]", "").split("\\s");
+            for(String word : wordsList) {
+                if(!uniqueWords.find(word)) {
+                    uniqueWords.insert(word);
+                }
+            }
+
+            documents.findNext();
+        }
+
+        Document doc = documents.retrieve();
+        String content = doc.getContent();
+        String[] wordsList = content.toLowerCase().replaceAll("[^a-zA-Z0-9\\s+]", "").split("\\s");
+        for(String word : wordsList) {
+            if(!uniqueWords.find(word)) {
+                uniqueWords.insert(word);
+            }
+        }
+
+        totalUniqueTokens = uniqueWords.size();
+
+        System.out.println("Number of unique tokens: " + totalUniqueTokens);
+    }
+
+    // This method is used to print the documents in the list - O(n)
+    static public void printDocuments(LinkedList<Document> documents) {
+        documents.findFirst();
+        while (!documents.last()) {
+            System.out.println("Document Id: " + documents.retrieve().getDocId());
+            documents.findNext();
+            System.out.println();
+        }
+        System.out.println("Document Id: " + documents.retrieve().getDocId());
     }
 }
