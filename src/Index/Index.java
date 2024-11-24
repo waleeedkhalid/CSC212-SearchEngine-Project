4f5024f3e788439db1298c761aa611b0954e5440
waleeedkhalid <axd.edits@gmail.com>
2024-11-24 19:01:24 +0300

    Index, InvertedIndex

--- src/Index/Index.java
+++ src/Index/Index.java
@@ -1,35 +1,89 @@
 package Index;
 
 import Process.*;
+import List.*;
 
 public class Index {
-    Document[] indexes;
+    private LinkedList<Document> documents;
 
     public Index() {
-        indexes = new Document [100];
+        documents = new LinkedList<Document>();
     }
 
-    public void addDocument(Document document) {
-        for (int i = 0; i < indexes.length; i++) {
-            if (indexes[i] == null) {
-                indexes[i] = document;
-                break;
-            }
+    public void addDocument(Document doc) {
+        documents.insert(doc);
+    }
+
+    public void setDocuments(LinkedList<Document> documents) {
+        this.documents = documents;
+    }
+
+    public LinkedList<Document> getDocuments() {
+        return documents;
+    }
+
+    public void displayDocuments() {
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            System.out.println(doc.toString());
+            documents.findNext();
         }
+        Document doc = documents.retrieve();
+        System.out.println(doc.toString());
     }
 
-    public Document[] getDocuments() {
-        return indexes;
-    }
-
-    public Document searchInformation(String word) {
-        for (Document document : indexes) {
-            if (document != null) {
-                if (document.getWords().equals(word)) {
-                    return document;
+    // This method is used to find word documents in the index - O(n^2)
+    public LinkedList<Document> find(String word) {
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            LinkedList<Word> words = doc.getWords();
+            words.findFirst();
+            while (!words.last()) {
+                Word w = words.retrieve();
+                if (w.getWord().equals(word)) {
+                    return w.getDocs();
                 }
+                words.findNext();
             }
+            Word w = words.retrieve();
+            if (w.getWord().equals(word)) {
+                return w.getDocs();
+            }
+            documents.findNext();
+        }
+        Document doc = documents.retrieve();
+        LinkedList<Word> words = doc.getWords();
+        words.findFirst();
+        while (!words.last()) {
+            Word w = words.retrieve();
+            if (w.getWord().equals(word)) {
+                return w.getDocs();
+            }
+            words.findNext();
+        }
+        Word w = words.retrieve();
+        if (w.getWord().equals(word)) {
+            return w.getDocs();
         }
         return null;
+    }
+
+    // Ranked Retrieval
+    public void rankedRetrieval(String query) {
+        // 1. Tokenize query
+        // 2. For each token, get the document frequency
+        // 3. For each token, get the term frequency
+        // 4. Calculate the weight of each token
+        // 5. Calculate the weight of the document
+        // 6. Calculate the similarity of the document
+        // 7. Sort the documents by similarity
+        // 8. Display the documents
+    }
+
+    // Boolean Retrieval
+    public void booleanRetrieval(String query) {
+
     }
 }
--- src/Index/InvertedIndex.java
+++ src/Index/InvertedIndex.java
@@ -1,151 +1,67 @@
 package Index;
 
 import Process.*;
-import List.LinkedList;
+import List.*;
 
 public class InvertedIndex {
-
-    LinkedList<Word> invertedindex;
+    private LinkedList<Word> words;
 
     public InvertedIndex() {
-        invertedindex = new LinkedList <Word>();
+        words = new LinkedList<Word>();
     }
 
-    public boolean addNew (Document doc) {
-        if (invertedindex.empty()) {
-            Word word = new Word(doc.getWords().retrieve().getWord(), doc);
-            word.addDoc(doc);
-            invertedindex.insert(word);
-            return true;
-        }
-
-        invertedindex.findFirst();
-        for (int i = 0; i < invertedindex.size(); i++) {
-            if (invertedindex.retrieve().getWord().compareTo(doc.getWords().retrieve().getWord()) == 0) {
-                invertedindex.retrieve().addDoc(doc);
-                return true;
+    public void addDocument(Document doc) {
+        LinkedList<Word> docWords = doc.getWords();
+        docWords.findFirst();
+        while (!docWords.last()) {
+            Word word = docWords.retrieve();
+            if (!words.find(word)) {
+                words.insert(word);
             }
-            invertedindex.findNext();
+            docWords.findNext();
         }
-
-        Word word = new Word(doc.getWords().retrieve().getWord(), doc);
-        word.addDoc(doc);
-        invertedindex.insert(word);
-        return true;
+        Word word = docWords.retrieve();
+        if (!words.find(word)) {
+            words.insert(word);
+        }
     }
 
-    public boolean found(String word)
-    {
-        if (invertedindex.empty())
-            return false;
-
-        invertedindex.findFirst();
-        for ( int i = 0 ; i < invertedindex.size() ; i++)
-        {
-            if ( invertedindex.retrieve().getWord().compareTo(word) == 0)
-                return true;
-            invertedindex.findNext();
+    public void addDocuments(LinkedList<Document> documents) {
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            addDocument(doc);
+            documents.findNext();
         }
-        return false;
+        Document doc = documents.retrieve();
+        addDocument(doc);
     }
 
-    public boolean [] ANDORFunction(String str )
-    {
-        if (! str.contains(" OR ") && ! str.contains(" AND "))
-        {
-            boolean [] r1 = new boolean[50];
-            str = str.toLowerCase().trim();
-
-            if (this.found (str))
-                r1 = this.invertedindex.retrieve().getDocs();
-            return r1;
+    public void displayWords() {
+        words.findFirst();
+        while (!words.last()) {
+            Word word = words.retrieve();
+            System.out.println(word.toString());
+            words.findNext();
         }
+        Word word = words.retrieve();
+        System.out.println(word.toString());
+    }
 
-        else if (str.contains(" OR ") && str.contains(" AND "))
-        {
-            String [] AND_ORs = str.split(" OR ");
-            boolean []  r1 = ANDFunction(AND_ORs[0]);
-
-            for ( int i = 1 ; i < AND_ORs.length ; i++  )
-            {
-                boolean [] r2 = ANDFunction(AND_ORs[i]);
-
-                for ( int j = 0 ; j < 50 ; j++ )
-                    r1 [j] = r1[j] || r2[j];
+    // This method is used to find word documents in the inverted index - O(n)
+    public LinkedList<Document> find(String word) {
+        words.findFirst();
+        while (!words.last()) {
+            Word w = words.retrieve();
+            if (w.getWord().equals(word)) {
+                return w.getDocs();
             }
-            return r1;
+            words.findNext();
         }
-
-        else  if (str.contains(" AND "))
-            return ANDFunction(str);
-
-        return ORFunction(str);
-    }
-
-    public boolean [] ANDFunction(String str)
-    {
-        String [] ANDs = str.split(" AND ");
-        LinkedList<Document> b1 = new LinkedList<Document>();
-
-        if (this.found (ANDs[0].toLowerCase().trim()))
-            b1 = this.invertedindex.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ANDs.length ; i++)
-        {
-            LinkedList<Document> b2 = new LinkedList<Document>();
-            if (this.found (ANDs[i].toLowerCase().trim()))
-                b2 = this.invertedindex.retrieve().getDocs();
-
-            for (int j = 0; j < b1.size(); j++) {
-                if (b2.find(b1.retrieve())) {
-                    b1.insert(b1.retrieve());
-                }
-                b1.findNext();
-            }
+        Word w = words.retrieve();
+        if (w.getWord().equals(word)) {
+            return w.getDocs();
         }
-
-        boolean [] r = new boolean[50];
-        for (int i = 0; i < b1.size(); i++) {
-            r[b1.retrieve().getDocId()] = true;
-            b1.findNext();
-        }
-        return r;
-    }
-
-    public boolean [] ORFunction(String str)
-    {
-        String [] ORs = str.split(" OR ");
-        boolean [] b1 = new boolean [50];
-
-        if (this.found (ORs[0].toLowerCase().trim()))
-            b1 = this.invertedindex.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ORs.length ; i++)
-        {
-            boolean [] b2 = new boolean [50];
-            if (this.found (ORs[i].toLowerCase().trim()))
-                b2 = this.invertedindex.retrieve().getDocs();
-
-            for ( int j = 0 ; j < 50 ; j++)
-                b1 [j] = b1[j] || b2[j];
-
-        }
-        return b1;
-    }
-
-    public void printDocment()
-    {
-        if (this.invertedindex.empty())
-            System.out.println("Empty Inverted Index");
-        else
-        {
-            this.invertedindex.findFirst();
-            while ( ! this.invertedindex.last())
-            {
-                System.out.println(invertedindex.retrieve());
-                this.invertedindex.findNext();
-            }
-            System.out.println(invertedindex.retrieve());
-        }
+        return null;
     }
 }
