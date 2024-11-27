4d9f9a5cd1dbff907d4a67e9821b580fea2b964d
waleeedkhalid <axd.edits@gmail.com>
2024-11-27 20:34:20 +0300

    Created InvertedIndexAVL

--- src/Index/InvertedIndexAVL.java
+++ src/Index/InvertedIndexAVL.java
@@ -1,103 +1,54 @@
 package Index;
 
 import Process.*;
+import List.*;
 import Trees.AVL.AVLTree;
+import Trees.BST.Order;
 
 public class InvertedIndexAVL {
-
-    AVLTree invertedindexAVL;
+    private AVLTree words;
 
     public InvertedIndexAVL() {
-        invertedindexAVL = new AVLTree();
+        words = new AVLTree();
     }
 
-    public boolean addNew (Document doc) {
-
-    }
-
-    public boolean found(String word)
-    {
-        return invertedindexAVL.find(root, word);
-    }
-
-    public void printDocument()
-    {
-        invertedindexAVL.Traverse();
-    }
-
-
-    public boolean [] ANDORFunction(String str )
-    {
-        if (! str.contains(" OR ") && ! str.contains(" AND "))
-        {
-            boolean [] r1 = new boolean[50];
-            str = str.toLowerCase().trim();
-
-            if (this.found (str))
-                r1 =  this.invertedindexAVL.retrieve().getDocs();
-            return r1;
-        }
-
-        else if (str.contains(" OR ") && str.contains(" AND "))
-        {
-            String [] AND_ORs = str.split(" OR ");
-            boolean []  r1 = ANDFunction(AND_ORs[0]);
-
-            for ( int i = 1 ; i < AND_ORs.length ; i++  )
-            {
-                boolean [] r2 = ANDFunction(AND_ORs[i]);
-
-                for ( int j = 0 ; j < 50 ; j++ )
-                    r1 [j] = r1[j] || r2[j];
+    public void addDocument(Document doc) {
+        LinkedList<Word> docWords = doc.getWords();
+        docWords.findFirst();
+        while (!docWords.last()) {
+            Word word = docWords.retrieve();
+            if (!words.find(word.getWord())) {
+                words.insert(word);
             }
-            return r1;
+            docWords.findNext();
         }
-
-        else  if (str.contains(" AND "))
-            return ANDFunction(str);
-
-        return ORFunction(str);
+        Word word = docWords.retrieve();
+        if (!words.find(word.getWord())) {
+            words.insert(word);
+        }
     }
 
-    public boolean [] ANDFunction(String str)
-    {
-        String [] ANDs = str.split(" AND ");
-        boolean [] b1 = new boolean [50];
-
-        if (this.found (ANDs[0].toLowerCase().trim()))
-            b1 = this.invertedindexAVL.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ANDs.length ; i++)
-        {
-            boolean [] b2 = new boolean [50];
-            if (this.found (ANDs[i].toLowerCase().trim()))
-                b2 = this.invertedindexAVL.retrieve().getDocs();
-
-            for ( int j = 0 ; j < 50 ; j++)
-                b1 [j] = b1[j] && b2[j];
+    public void addDocuments(LinkedList<Document> documents) {
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            addDocument(doc);
+            documents.findNext();
         }
-        return b1;
+        Document doc = documents.retrieve();
+        addDocument(doc);
     }
 
-    public boolean [] ORFunction(String str)
-    {
-        String [] ORs = str.split(" OR ");
-        boolean [] b1 = new boolean [50];
-
-        if (this.found (ORs[0].toLowerCase().trim()))
-            b1 = this.invertedindexAVL.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ORs.length ; i++)
-        {
-            boolean [] b2 = new boolean [50];
-            if (this.found (ORs[i].toLowerCase().trim()))
-                b2 = this.invertedindexAVL.retrieve().getDocs();
-
-            for ( int j = 0 ; j < 50 ; j++)
-                b1 [j] = b1[j] || b2[j];
-
-        }
-        return b1;
+    public void displayWords() {
+        words.traverse(Order.INORDER);
     }
 
+    // Returns the list of documents that contain the word - O(log n)
+    public LinkedList<Document> find(String word) {
+        return words.search(word).getDocs();
+    }
+
+    public AVLTree getWords() {
+        return words;
+    }
 }
