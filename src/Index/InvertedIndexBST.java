3c5442031ca61e1c402a4653c828a09bfe25b0d4
waleeedkhalid <axd.edits@gmail.com>
2024-11-27 20:34:15 +0300

    Created InvertedIndexBST

--- src/Index/InvertedIndexBST.java
+++ src/Index/InvertedIndexBST.java
@@ -1,123 +1,58 @@
 package Index;
 
 import Process.*;
-import Trees.*;
+import List.*;
 import Trees.BST.BST;
-
+import Trees.BST.Order;
 
 public class InvertedIndexBST {
-    BST<Word> invertedindexBST;
-    int count = 0;
+    private BST words;
 
     public InvertedIndexBST() {
-        invertedindexBST = new BST<> ();
+        words = new BST();
     }
 
-    public boolean addNew (Document doc)
-    {
-        String [] words = doc.getContent().split(" ");
-        for ( int i = 0 ; i < words.length ; i++)
-        {
-            words[i] = words[i].toLowerCase().trim();
-            if (this.found(words[i]))
-            {
-                Word word = invertedindexBST.retrieve();
-                word.addDoc(doc);
-                invertedindexBST.update(words[i]);
+    // This method is used to add a document to the inverted index with BST - O(n)
+    public void addDocument(Document doc) {
+        LinkedList<Word> docWords = doc.getWords();
+        docWords.findFirst();
+        while (!docWords.last()) {
+            Word word = docWords.retrieve();
+            if (!words.search(word)) {
+                words.insert(word);
             }
-            else
-            {
-                Word word = new Word(words[i]);
-                word.addDoc(doc);
-                invertedindexBST.insert(words[i]);
-            }
+            docWords.findNext();
         }
-        return true;
-    }
-
-    public boolean found(String word)
-    {
-        return invertedindexBST.find(word);
-    }
-
-    public void printDocument()
-    {
-        invertedindexBST.Traverse();
-    }
-    //=====================================================================
-    public boolean [] AND_OR_Function (String str )
-    {
-        if (! str.contains(" OR ") && ! str.contains(" AND "))
-        {
-            boolean [] r1 = new boolean[50];
-            str = str.toLowerCase().trim();
-
-            if (this.found (str))
-                r1 =  this.invertedindexBST.retrieve().getDocs();
-            return r1;
+        Word word = docWords.retrieve();
+        if (!words.search(word)) {
+            words.insert(word);
         }
-
-        else if (str.contains(" OR ") && str.contains(" AND "))
-        {
-            String [] AND_ORs = str.split(" OR ");
-            boolean []  r1 = ANDFunction(AND_ORs[0]);
-
-            for ( int i = 1 ; i < AND_ORs.length ; i++  )
-            {
-                boolean [] r2 = ANDFunction(AND_ORs[i]);
-
-                for ( int j = 0 ; j < 50 ; j++ )
-                    r1 [j] = r1[j] || r2[j];
-            }
-            return r1;
-        }
-
-        else  if (str.contains(" AND "))
-            return ANDFunction(str);
-
-        return ORFunction(str);
     }
 
-    public boolean [] ANDFunction(String str)
-    {
-        String [] ANDs = str.split(" AND ");
-        boolean [] b1 = new boolean [50];
-
-        if (this.found (ANDs[0].toLowerCase().trim()))
-            b1 = this.invertedindexBST.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ANDs.length ; i++)
-        {
-            boolean [] b2 = new boolean [50];
-            if (this.found (ANDs[i].toLowerCase().trim()))
-                b2 = this.invertedindexBST.retrieve().getDocs();
-
-            for ( int j = 0 ; j < 50 ; j++)
-                b1 [j] = b1[j] && b2[j];
+    public void addDocuments(LinkedList<Document> documents) {
+        documents.findFirst();
+        while (!documents.last()) {
+            Document doc = documents.retrieve();
+            addDocument(doc);
+            documents.findNext();
         }
-        return b1;
+        Document doc = documents.retrieve();
+        addDocument(doc);
     }
 
-    public boolean [] ORFunction(String str)
-    {
-        String [] ORs = str.split(" OR ");
-        boolean [] b1 = new boolean [50];
-
-        if (this.found (ORs[0].toLowerCase().trim()))
-            b1 = this.invertedindexBST.retrieve().getDocs();
-
-        for ( int i = 1 ; i< ORs.length ; i++)
-        {
-            boolean [] b2 = new boolean [50];
-            if (this.found (ORs[i].toLowerCase().trim()))
-                b2 = this.invertedindexBST.retrieve().getDocs();
-
-            for ( int j = 0 ; j < 50 ; j++)
-                b1 [j] = b1[j] || b2[j];
-
-        }
-        return b1;
+    public void displayWords() {
+        words.traverse(Order.INORDER);
     }
 
+    // This method is used to find word documents in the inverted index - O(log n) FASTER than InvertedIndex
+    public LinkedList<Document> find(String word) {
+        if (words.search(word)) {
+            return words.retrieve().getDocs();
+        }
+        return null;
+    }
 
+    public BST getWords() {
+        return words;
+    }
 }
